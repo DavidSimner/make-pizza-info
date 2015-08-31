9bc518333f6d86e2e4e20131f6e69d5c7b0ca774
David Simner <david.simner@red-gate.com>
2015-08-31 11:06:38 +0100

    add a header

--- app/main.js
+++ app/main.js
@@ -3,12 +3,17 @@ requirejs.config({
         text: '../node_modules/durandal/node_modules/requirejs-text/text',
         durandal: '../node_modules/durandal/js',
         jquery: '../node_modules/durandal/node_modules/jquery/dist/jquery.min',
-        knockout: '../node_modules/durandal/node_modules/knockout/build/output/knockout-latest'
+        knockout: '../node_modules/durandal/node_modules/knockout/build/output/knockout-latest',
+        plugins: '../node_modules/durandal/js/plugins'
     }
 });
 
 require(['durandal/app'], function (durandal) {
     durandal.title = 'Make Pizza!';
+
+    durandal.configurePlugins({
+        widget: true
+    });
 
     durandal.start().done(function () {
         durandal.setRoot('shell');
---
+++ app/widgets/header/viewmodel.js
@@ -0,0 +1,6 @@
+define([], function () {
+    function HeaderViewModel() {
+    }
+
+    return HeaderViewModel;
+});

41b64e8ac1fc0777cba11970d35fe8e9746e164e
David Simner <david.simner@red-gate.com>
2015-12-14 15:18:21 +0100

    make it clear what data comes back

--- app/modules/trace/MicrosoftAzureTraceProvider.js
+++ app/modules/trace/MicrosoftAzureTraceProvider.js
@@ -3,7 +3,16 @@ define(['xr'], function (xr) {
     }
 
     MicrosoftAzureTraceProvider.prototype.get = function () {
-        return xr.get('/trace.json');
+        return xr.get('/trace.json').then(function (value) {
+        	return {
+        		provider: value.provider,
+        		datacentre: value.datacentre,
+        		server: value.server,
+        		https: value.https,
+        		domain: value.domain,
+        		client: value.client
+        	};
+        });
     };
 
     return MicrosoftAzureTraceProvider;

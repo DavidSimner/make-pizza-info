4cb7a0e45e0d78f24c219655ad7737bb49c78774
David Simner <david.simner@red-gate.com>
2015-12-16 11:51:51 +0100

    give the html the thing

--- app/modules/trace/index.js
+++ app/modules/trace/index.js
@@ -1,9 +1,12 @@
-define(['modules/trace/MicrosoftAzureTraceProvider'], function (MicrosoftAzureTraceProvider) {
+define(['knockoutmapping', 'modules/trace/ViewModelGenerator'], function (mapping, ViewModelGenerator) {
     function TraceViewModel() {
     }
 
     TraceViewModel.prototype.activate = function () {
-        return new MicrosoftAzureTraceProvider().get();
+    	var self = this;
+        return new ViewModelGenerator().get().then(function (data) {
+        	mapping.fromJS(data, {}, self);
+        });
     };
 
     return TraceViewModel;

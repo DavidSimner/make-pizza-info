8b51460163b2a338c7177c0e180dc435e2fa1ed0
David Simner <david.simner@red-gate.com>
2016-01-03 21:42:18 +0000

    Revert "writing the file into the right place helps..."
    This reverts commit 9f59e61fb640215708186129750394d04b2a3ab6.

--- gulpfile.js
+++ gulpfile.js
@@ -142,7 +142,7 @@ function deploy (cwd, url, cb) {
     function setAttributes (err) {
         if (err) return cb(err);
 
-        fs.writeFileSync(cwd + '/.gitattributes', '* -text');
+        fs.writeFileSync('.gitattributes', '* -text');
 
         return add();
     }
